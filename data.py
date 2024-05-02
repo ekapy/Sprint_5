3dbd1f7dd3ac8105130e413839f1dd45d4590702
Kate Pyankova <pyankate@gmail.com>
2024-05-02 16:35:42 +0300

    commit_try_2_1

--- data.py
+++ data.py
@@ -5,7 +5,4 @@ def get_sign_in_data():
     password = '123456'
     return email, password
 
-def get_active_state_button():
-    active_button = 'tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect'
-    return active_button
 
--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -2,33 +2,30 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from config import URL
 from locators import BurgersLocators
-from data import get_active_state_button
+
 
 
 class TestConstructor():
     def test_active_buns(self, driver):
         driver.get(URL)
-        active_button = get_active_state_button()
 
         driver.find_element(*BurgersLocators.SAUCE).click()
         WebDriverWait(driver, 15).until(expected_conditions.visibility_of_element_located(BurgersLocators.SAUCE_CONTAINER))
 
         driver.find_element(*BurgersLocators.BUNS).click()
 
-        assert driver.find_element(*BurgersLocators.BUNS).get_attribute('class') == active_button, "Булки не выбраны"
+        assert driver.find_element(*BurgersLocators.BUNS).get_attribute('class') == BurgersLocators.ACTIVE_STATE_BUTTON, "Булки не выбраны"
 
     def test_active_sauce(self, driver):
         driver.get(URL)
-        active_button = get_active_state_button()
 
         WebDriverWait(driver,15).until(expected_conditions.visibility_of_element_located(BurgersLocators.SAUCE))
         driver.find_element(*BurgersLocators.SAUCE).click()
 
-        assert driver.find_element(*BurgersLocators.SAUCE).get_attribute('class') == active_button, "Соусы не выбраны"
+        assert driver.find_element(*BurgersLocators.SAUCE).get_attribute('class') == BurgersLocators.ACTIVE_STATE_BUTTON, "Соусы не выбраны"
 
     def test_active_fillings(self, driver):
         driver.get(URL)
-        active_button = get_active_state_button()
 
         #WebDriverWait(driver,15).until(expected_conditions.element_to_be_clickable(BurgersLocators.FILLINGS))
         driver.find_element(*BurgersLocators.FILLINGS).click()
